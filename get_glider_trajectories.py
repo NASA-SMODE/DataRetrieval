1e934a0254ba765607c585e33b1e5129a584e0fb
cesar-rocha <rocha.cesarb@protonmail.com>
2021-10-11 20:59:39 -0400

    Add script to retrieve average coordinates

--- get_glider_trajectories.py
+++ get_glider_trajectories.py
@@ -2,41 +2,67 @@ import numpy as np
 import xarray as xr
 from siphon.catalog import TDSCatalog
 
-# Settings 
-ids = ['ng231','ng258','ng268','ng316',
-       'ng350','ng533','ng599','ng665',
-       'ng734','ng780']
+def main():
 
-base_url = 'http://smode.whoi.edu:8080/thredds/dodsC/insitu/navo_glider/'
-cat = TDSCatalog('http://smode.whoi.edu:8080/thredds/catalog/insitu/navo_glider/catalog.html')
+    # Settings 
+    ids = ['ng231','ng258','ng268','ng316',
+        'ng350','ng533','ng599','ng665',
+        'ng734','ng780']
 
-# Get filelist and build list of datapaths
+    base_url = 'http://smode.whoi.edu:8080/thredds/dodsC/insitu/navo_glider/'
+    cat = TDSCatalog('http://smode.whoi.edu:8080/thredds/catalog/insitu/navo_glider/catalog.html')
 
-filelist=[file for file in 
-            cat.datasets if file.startswith(ids[0])]
+    # Loop over gliders and get averaged earth coordinates
+    for id in ids:
 
-datapath_nooptics = [base_url+file for file in 
-                        filelist if 'optics' not in file]
+        # Get filelist and build list of datapaths
+        filelist=[file for file in 
+                    cat.datasets if file.startswith(id)]
 
-# Get averaged earth coordinates of glider profiles
+        datapath_nooptics = [base_url+file for file in 
+                                filelist if 'optics' not in file]
 
-lat, lon = np.array([]), np.array([])
+        lat, lon = np.array([]), np.array([])
 
-# Data time is epoch time: since 00:00Z 1 January 1970
-start =  np.datetime64('1970-01-01') 
-time = []
+        # scitime = timedelta from 000Z 1/1/1970
+        start =  np.datetime64('1970-01-01') 
+        time = []
 
-for path in datapath_nooptics:
+        for path in sorted(datapath_nooptics):
+            
+            glider_data = xr.open_dataset(path)
+        
+            lat = np.hstack([lat, glider_data.latitude.mean().values])
+            lon = np.hstack([lon, glider_data.longitude.mean().values])
+            time.append(glider_data.scitime.mean().values + start)
+
+        # round time at the minute level
+        time = np.array(time).astype('datetime64[m]')   
+
+        # mask bad data
+        lon[lon>360] = np.nan
+        lat[lon>360] = np.nan 
     
-    glider_data = xr.open_dataset(path)
-  
-    lat = np.hstack([lat, glider_data.latitude.mean().values])
-    lon = np.hstack([lon, glider_data.longitude.mean().values])
-    time.append(glider_data.scitime.mean().values + start)
+        # put data into a Dataset
+        coordinates = {
+            'time': time, 
+            'glider': id
+        } 
+        variables = {
+            'longitude': ('time', lon),
+            'latitude': ('time', lat)
+        } 
 
-time = np.array(time)
+        ds = xr.Dataset(variables, coords=coordinates
+                    ).expand_dims('glider')
 
-# TODO: loop over gliders and put the data in a json file
+        if id is ids[0]:
+            glider_trajectories = ds 
+        else: 
+            glider_trajectories = xr.merge([glider_trajectories,ds])
 
-# if __name__ == "__main__":
-#     main()
+        # save trajectories to netCDF file
+        glider_trajectories.to_netcdf('output/glider_trajectories.nc')
+
+if __name__ == "__main__":
+    main()
